19b867180b4d893f7d91123c2562e609c423146e
Nathan <nathan@Nathans-MacBook-Pro.local>
2019-06-25 15:14:46 +0100

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <h2>Hello World</h2>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
